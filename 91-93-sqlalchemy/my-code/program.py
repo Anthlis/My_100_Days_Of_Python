111b37d0143b5e0f7a1fbb95177d108580805521
Anthony Lister <anthony.lister@gmail.com>
2019-07-07 21:10:21 +1200

    Added a dict menu to inventory program. WIP.

---
+++ 91-93-sqlalchemy/my-code/program.py
@@ -0,0 +1,26 @@
+import sqlalchemy
+
+def main():
+    pass
+
+
+def header():
+    pass
+
+
+def add_flight():
+    pass
+
+
+def type_time():
+    pass
+
+
+def total_time():
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
+
